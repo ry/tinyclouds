ffa635b337a7a4eb30ea2213e7e62011723a6efa
Ryan Dahl <ry@tinyclouds.org>
2022-05-01 17:55:09 -0400

    fmt

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ import blog from "./src/blog.tsx";
 blog(import.meta.url, {
   title: "Ryan Dahl",
   subtitle: "",
-  header:
-    `![](http://tinyclouds.org/ry.jpg)
+  header: `![](http://tinyclouds.org/ry.jpg)
      ry@tinyclouds.org`,
   gaKey: "UA-91675022-1",
 });
