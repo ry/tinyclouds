a994255d01b8d0aefd802e422b70b93fd12775bc
Ryan Dahl <ry@tinyclouds.org>
2024-03-26 10:43:15 -0400

    add twitter link

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ blog({
   links: [
     { title: "Email", url: "mailto:ry@tinyclouds.org" },
     { title: "GitHub", url: "https://github.com/ry" },
+    { title: "Twitter", url: "https://twitter.com/rough__sea" },
   ],
   background: "#fff",
   middlewares: [
